07af5f892ad976465dc04de5a5fe2453e41e5590
oploid <oploid7@gmail.com>
2015-02-04 16:34:11 +0000

    Fixed link to about page

--- rango/views.py
+++ rango/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("<html><body><h1>Rango says hey there, world!</h1><a href='/rango/about/>About</a></body></html>")
+	return HttpResponse("<html><body><h1>Rango says hey there, world!</h1><p><a href='/rango/about/'>About</a></p></body></html>")
 
 def about(request):
 	return HttpResponse("<html><body><h1>This is the about page.</h1><a href='/rango/'>Home</a></body></html>")
