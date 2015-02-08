40ba97649c4b69f972ec860d90d1ab3f7af51a53
oploid <oploid7@gmail.com>
2015-02-08 18:01:44 +0000

    Chapter 5.1 - Templates

--- rango/views.py
+++ rango/views.py
@@ -1,7 +1,11 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-	return HttpResponse("<html><body><h1>Rango says hey there, world!</h1><p><a href='/rango/about/'>About</a></p></body></html>")
+	# Construct a dictionary to pass to the template engine as its context.
+	context_dict = {'boldmessage':"I am bold font from the context"}
+	#Return a rendered response to send to the client.
+	return render(request, 'rango/index.html', context_dict)
 
 def about(request):
 	return HttpResponse("<html><body><h1>This is the about page.</h1><a href='/rango/'>Home</a></body></html>")
