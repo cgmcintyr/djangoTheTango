9bd16d9f5bb0946cf19382764151b140a9d4e332
oploid <oploid7@gmail.com>
2015-02-15 11:11:27 +0000

    Replaced all tabs with spaces

--- rango/views.py
+++ rango/views.py
@@ -2,12 +2,12 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-	# Construct a dictionary to pass to the template engine as its context.
-	context_dict = {'boldmessage':"I am bold font from the context"}
-	#Return a rendered response to send to the client.
-	return render(request, 'rango/index.html', context_dict)
+    # Construct a dictionary to pass to the template engine as its context.
+    context_dict = {'boldmessage':"I am bold font from the context"}
+    #Return a rendered response to send to the client.
+    return render(request, 'rango/index.html', context_dict)
 
 def about(request):
-	context_dict = {}
-	return render(request, 'rango/about.html', context_dict)
+    context_dict = {}
+    return render(request, 'rango/about.html', context_dict)
 
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -8,11 +8,11 @@ urlpatterns = patterns('',
 )
 
 if settings.DEBUG:
-	urlpatterns += patterns(
-		'django.views.static', 
-		(r'^media/(?P<path>.*)',
-		'serve',
-		{'document_root': settings.MEDIA_ROOT}), 
-	)
+    urlpatterns += patterns(
+        'django.views.static', 
+        (r'^media/(?P<path>.*)',
+        'serve',
+        {'document_root': settings.MEDIA_ROOT}), 
+    )
 
 
