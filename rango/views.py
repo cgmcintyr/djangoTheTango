710d322aca534ffadd1fb09c7aa2e85641ffecca
oploid <oploid7@gmail.com>
2015-02-19 18:57:53 +0000

    Added list of categories to rango's index page

--- rango/views.py
+++ rango/views.py
@@ -1,9 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from rango.models import Category
 
 def index(request):
     # Construct a dictionary to pass to the template engine as its context.
-    context_dict = {'boldmessage':"I am bold font from the context"}
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
     #Return a rendered response to send to the client.
     return render(request, 'rango/index.html', context_dict)
 
