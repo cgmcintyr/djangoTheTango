a6a554fe392e0f7eb09f7249693791a6aea1561f
oploid <oploid7@gmail.com>
2015-02-23 19:37:58 +0000

    8. Added 'add_category' view

--- rango/views.py
+++ rango/views.py
@@ -14,6 +14,30 @@ def about(request):
     context_dict = {}
     return render(request, 'rango/about.html', context_dict)
 
+def add_category(request):
+    # A HTTP POST?
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        
+        # Have we provided with a valid form?
+        if form.is_valid():
+            # Save the new category to the database.
+            form.save(commit=True)
+
+            # Now call the index() view.
+            # The user will be shown the homepage.
+            return index(request)
+        else:
+            # The supplied form contained errors - print them to terminal
+            print form.errors
+    else:
+        # If the request was not a POST, display the form to enter details.
+        form = CategoryForm()
+
+    # Bad form (or form details), no form supplied...
+    # Render the form with error messages (if any)
+    return render(request, 'rango/add_category.html', {'form': form})
+
 def category(request, category_name_slug):
     # Create a context dictionary which we can pass to the template rendering engine
     context_dict = {}
